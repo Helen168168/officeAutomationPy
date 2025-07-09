b691c56571789521ae53cd264ee94acff22333e4
pxm168168 <1093232495@qq.com>
2025-07-09 18:48:05 +0800

    feat: 面向对象编程的案例和作业

--- worstCode.py
+++ worstCode.py
@@ -1,56 +1,28 @@
-'''
-实现一个银行账户系统，包含账户余额敏感数据，也希望限制直接修改余额和隐藏内部计算逻辑
-'''
-class BankAccount:
-    def __init__(self, balance=0, pwd='123456'):
-        self._balance = balance  # 使用单下划线表示这是一个受保护的属性
-        self.__password = pwd
-    def __verifyPassword(self, pwd):  # 私有方法
-        """密码验证"""
-        return self.__password == pwd
-    def deposit(self, amount):
-        if amount <= 0:
-            raise ValueError("存款金额必须大于零")
-        self._balance += amount
+class SmartLock:
+    def __init__(self):
+        self.__password = "123456"
+        self.__is_locked = True
+        self.__attempts = 0
 
-    def withdraw(self, amount):
-        if amount <= 0:
-            raise ValueError("取款金额必须大于零")
-        if amount > self._balance:
-            raise ValueError("余额不足")
-        self._balance -= amount
+    def unlock(self, password):
+        if self.__attempts >= 3:
+            print("尝试次数过多，已锁定")
+            return
 
-    def getBalance(self):
-        return self._balance
-    def _calculateInterest(self, rate):
-        """计算利息的内部方法，不应该被外部调用"""
-        return self._balance * rate
-    def applyInterest(self, rate):
-        """应用利息到账户余额"""
-        interest = self._calculateInterest(rate)
-        self._balance += interest
-        return interest
+        if password == self.__password:
+            self.__is_locked = False
+            self.__attempts = 0
+            print("门已开锁")
+        else:
+            self.__attempts += 1
+            print(f"密码错误，剩余尝试次数：{3 - self.__attempts}")
 
-# 测试代码
-if __name__ == "__main__":
-    account = BankAccount(100)
-    print("初始余额:", account.getBalance())
-    print(dir(account))
-    print(account.__password)
-
-    account.deposit(50)
-    print("存款后余额:", account.getBalance())
-
-    try:
-        account.withdraw(200)
-    except ValueError as e:
-        print(e)
-
-    account.withdraw(30)
-    print("取款后余额:", account.getBalance())
-
-    interest = account.applyInterest(0.05)
-    print("应用利息后余额:", account.getBalance(), "利息:", interest)
-
-    # 尝试直接访问受保护属性
-    print("直接访问受保护属性（不推荐）:", account._balance)  # 这不是一个好习惯
+    def changePassword(self, old_pass, new_pass):
+        if old_pass == self.__password:
+            self.__password = new_pass
+            print("密码修改成功")
+        else:
+            print("原密码错误")
+smartLock = SmartLock()
+smartLock.unlock("123456")  # 正确密码，门已开锁
+smartLock.changePassword("123456", "654321")  # 修改密码
